00170354230a7b4c051e3368b42e7aca7e982e6b
EvgeniyRuban <84354312+EvgeniyRuban@users.noreply.github.com>
2024-04-20 16:42:59 +0600

    Создание базовых сущностей

---
+++ IntravisionTestTask.Domain/Entities/IEntity.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IntravisionTestTask.Domain.Entities
+{
+    public interface IEntity<TKey> where TKey : struct
+    {
+        public TKey Id { get; set; }
+    }
+
+    public class ProductMachine : IEntity<Guid>
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public ICollection<ProductSlot> ProductSlots { get; set; }
+    }
+}
