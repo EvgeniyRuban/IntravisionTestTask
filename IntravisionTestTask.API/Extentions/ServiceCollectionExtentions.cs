6151b55ac57eaa6c7cc29e305126d39d6b4c6f7a
EvgeniyRuban <84354312+EvgeniyRuban@users.noreply.github.com>
2024-04-20 18:39:06 +0600

    Перенос конфигурации DbConnection в extention метод

---
+++ IntravisionTestTask.API/Extentions/ServiceCollectionExtentions.cs
@@ -0,0 +1,21 @@
+﻿using IntravisionTestTask.DAL.EF;
+using IntravisionTestTask.Domain.Definitions;
+using Microsoft.EntityFrameworkCore;
+
+namespace IntravisionTestTask.API.Extentions
+{
+    public static class ServiceCollectionExtentions
+    {
+        public static void ConfigureDbContext(this IServiceCollection services, WebApplicationBuilder builder)
+        {
+            var sqlServerProvider = builder.Configuration.GetValue<string>(Constants.Configurations.SQLProviderKey);
+            var connectionString = builder.Configuration.GetConnectionString(sqlServerProvider);
+            var migrationAssembly = string.Concat(Constants.CommonAssemblyName, '.', sqlServerProvider);
+
+            builder.Services.AddDbContext<ApplicationContext>(options =>
+            {
+                options.UseNpgsql(connectionString, o => o.MigrationsAssembly(migrationAssembly));
+            });
+        }
+    }
+}
--- IntravisionTestTask.API/Program.cs
+++ IntravisionTestTask.API/Program.cs
@@ -1,20 +1,11 @@
-using IntravisionTestTask.DAL.EF;
-using IntravisionTestTask.Domain.Definitions;
-using Microsoft.EntityFrameworkCore;
+using IntravisionTestTask.API.Extentions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var sqlServerProvider = builder.Configuration.GetValue<string>(Constants.Configurations.SQLProviderKey);
-var connectionString = builder.Configuration.GetConnectionString(sqlServerProvider);
-var migrationAssembly = string.Concat(Constants.CommonAssemblyName, '.', sqlServerProvider);
-
-builder.Services.AddDbContext<ApplicationContext>(options =>
-{
-    options.UseNpgsql(connectionString, o => o.MigrationsAssembly(migrationAssembly));
-});
+builder.Services.ConfigureDbContext(builder);
 
 var app = builder.Build();
 
